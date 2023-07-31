4504c26990b394747119d1911e857402a0ebafda
쿠로쿠치 <thsckdghks13@naver.com>
2023-07-31 12:23:30 +0900

    ttttt3

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -3,10 +3,10 @@ import { ObjectId } from 'mongodb';
 
 export default async function Detail({params}:any) {
 
-    const client = await connectDB
-    const db = client.db('forum')
-
-    let result = await db.collection('list').findOne({_id: new ObjectId(params.id)})
+    // const client = await connectDB
+    // const db = client.db('forum')
+    
+    // let result = await db.collection('list').findOne({_id: new ObjectId(params.id)})
 
     return (
         <div className="bg-slate-100 p-5 h-screen">
@@ -14,12 +14,12 @@ export default async function Detail({params}:any) {
                 <h4 className='text-lg font-bold m-0'>
                     상세페이지
                 </h4>
-                <h4 className='text-lg font-bold m-0'>
+                {/* <h4 className='text-lg font-bold m-0'>
                     {result && result.title}
                 </h4>
                 <p className='text-gray-600 m-1'>
                     {result && result.date}
-                </p>
+                </p> */}
             </div>
         </div>
     )
